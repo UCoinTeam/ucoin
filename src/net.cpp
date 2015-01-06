00c4a7adb68c8379f0247dff6f98815002d30194
UCoinTeam <support@ucoin.name>
2015-01-06 07:44:40 +0300

    Added node

--- src/net.cpp
+++ src/net.cpp
@@ -1141,6 +1141,7 @@ void MapPort()
 static const char *strDNSSeed[][2] = {
     {"node1.ucoin.name", "node1.ucoin.name"},
     {"node2.ucoin.name", "node2.ucoin.name"},
+    {"ucoin.suprnova.cc", "ucoin.suprnova.cc"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
