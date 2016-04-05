2487b37f32cb911dc1015072a196c55855801f4e
UCoinTeam <support@ucoin.name>
2016-04-06 00:44:31 +0300

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1139,10 +1139,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"node1.ucoin.name", "node1.ucoin.name"},
-    {"node2.ucoin.name", "node2.ucoin.name"},
-    //{"node3.ucoin.name", "node3.ucoin.name"},
-    //{"ucoin.suprnova.cc", "ucoin.suprnova.cc"},
+    {"159.203.12.191", "159.203.12.191"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
